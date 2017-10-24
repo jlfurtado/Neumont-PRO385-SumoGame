8d55e8aa0f74aa566bba5fa655976d4a58bac829
Justin Furtado <justin.l.furtado@gmail.com>
2017-10-24 09:40:09 -0600

    Temp basic fun pushing

---
+++ TwoPlayerGame/Assets/Scripts/Player1Controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player1Controller : MonoBehaviour {
+    [SerializeField] [Range(0.0f, 1000.0f)] private float Speed = 10.0f;
+
+    private Rigidbody myRigidBody;
+
+	// Use this for initialization
+	void Awake () {
+        myRigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float dx = (Input.GetKey(KeyCode.A) ? -1.0f : 0.0f) + (Input.GetKey(KeyCode.D) ? 1.0f : 0.0f);
+        float dz = (Input.GetKey(KeyCode.W) ? 1.0f : 0.0f) + (Input.GetKey(KeyCode.S) ? -1.0f : 0.0f);
+
+        myRigidBody.AddForce(new Vector3(dx, 0.0f, dz) * Speed);
+    }
+}
