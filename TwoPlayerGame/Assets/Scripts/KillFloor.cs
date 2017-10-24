687a698fb4efea33036f589a0bfcece84415de01
Justin Furtado <justin.l.furtado@gmail.com>
2017-10-24 14:57:57 -0600

    Basic win-lose-tie condition code, todo upgrade

--- TwoPlayerGame/Assets/Scripts/KillFloor.cs
+++ TwoPlayerGame/Assets/Scripts/KillFloor.cs
@@ -1,16 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KillFloor : MonoBehaviour {
+    [SerializeField] private GameObject m_gameOverRef;
+    [SerializeField] private float m_winCheckDelay;
+    private Text m_gameOverText;
+
+    private bool m_p1Hit;
+    private bool m_p2Hit;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+        m_gameOverText = m_gameOverRef.GetComponentInChildren<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player1"))
+        {
+            m_p1Hit = true;
+            StartCoroutine(CheckWinCondition());
+        }
+        else if (collision.collider.CompareTag("Player2"))
+        {
+            m_p2Hit = true;
+            StartCoroutine(CheckWinCondition());
+        }
+    }
+
+    private IEnumerator CheckWinCondition()
+    {
+        yield return new WaitForSeconds(m_winCheckDelay);
+        PauseManager.OnlyOne.Pause();
+        m_gameOverRef.SetActive(true);
+        m_gameOverText.text = (m_p1Hit) ? (m_p2Hit ? "Draw!" : "P1 Wins!") : (m_p2Hit ? "P2 Wins!" : "THIS IS A BUG!");
+    }
 }
