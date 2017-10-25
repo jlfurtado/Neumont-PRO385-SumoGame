9fa9bab84cc5e0d0c23ab093a171919518c12ae7
Sen <keima.ayasaki@gmail.com>
2017-10-25 09:39:11 -0600

    Basic Jump powerup

---
+++ twoplayergame/Assets/Scripts/PowerUps/PowerUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour {
+
+    protected Rigidbody rigidbody_PlayerOne;
+    protected Rigidbody rigidbody_PlayerTwo;
+
+    private void Awake()
+    {
+        rigidbody_PlayerOne = GameObject.FindGameObjectWithTag("Player1").GetComponent<Rigidbody>();
+        rigidbody_PlayerTwo = GameObject.FindGameObjectWithTag("Player2").GetComponent<Rigidbody>();
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        // TODO:: make better
+        if (collision.gameObject.CompareTag("Player1"))
+        {
+            CauseEffect(rigidbody_PlayerOne);
+        }
+        else if (collision.gameObject.CompareTag("Player2"))
+        {
+            CauseEffect(rigidbody_PlayerTwo);
+        }
+    }
+
+    public virtual void CauseEffect(Rigidbody otherRigidBody) { }
+}
---
+++ twoplayergame/Assets/Scripts/PowerUps/PowerUp_Jump.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PowerUp_Jump : PowerUp
+{
+    [SerializeField] [Range(0.0f, 1000.0f)] private float jumpForce = 500.0f;
+
+    public override void CauseEffect(Rigidbody otherRigidBody)
+    {
+        otherRigidBody.AddForce(new Vector3(0.0f, jumpForce, 0.0f));
+        this.gameObject.SetActive(false);
+    }
+}
