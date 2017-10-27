0d74a06837586eb16843d9d335a907783a73f252
Sen <keima.ayasaki@gmail.com>
2017-10-27 08:38:44 -0600

    Added a basic power up manager

---
+++ twoplayergame/Assets/Scripts/PowerUps/PowerUpManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpManager : MonoBehaviour {
+
+    public GameObject[] powerUpPrefabList;
+    public const int MAX_POWERUPS = 10;
+
+    private GameObject[] powerUps = new GameObject[MAX_POWERUPS];
+    private int powerUpTracker = 0;
+
+	// Use this for initialization
+	void Start () {
+		for(int j = 0; j < MAX_POWERUPS;) {
+            powerUps[j] = Instantiate(GetRandomPrefab(), this.transform);
+        }
+	}
+
+    GameObject GetRandomPrefab() {
+        return powerUpPrefabList[Random.Range(0, powerUpPrefabList.Length)];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
