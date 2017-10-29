7fa9347c04c3c4bc4f35e344080745f7aa32ebe4
Justin Furtado <justin.l.furtado@gmail.com>
2017-10-29 12:30:13 -0600

    Work on powerup manager

--- twoplayergame/Assets/Scripts/PowerUps/PowerUp.cs
+++ twoplayergame/Assets/Scripts/PowerUps/PowerUp.cs
@@ -6,6 +6,8 @@ public class PowerUp : MonoBehaviour {
 
     protected Rigidbody rigidbody_PlayerOne;
     protected Rigidbody rigidbody_PlayerTwo;
+    protected PowerUpManager m_myManager;
+    protected int m_myManagerIndex;
 
     private void Awake()
     {
@@ -13,16 +15,25 @@ public class PowerUp : MonoBehaviour {
         rigidbody_PlayerTwo = GameObject.FindGameObjectWithTag("Player2").GetComponent<Rigidbody>();
     }
 
+    public void HookIntoManager(PowerUpManager myManagerRef, string name, int idx)
+    {
+        m_myManager = myManagerRef;
+        this.name = name;
+        m_myManagerIndex = idx;
+    }
+    
     public void OnCollisionEnter(Collision collision)
     {
         // TODO:: make better
         if (collision.gameObject.CompareTag("Player1"))
         {
             CauseEffect(rigidbody_PlayerOne);
+            m_myManager.CollectAt(m_myManagerIndex);
         }
         else if (collision.gameObject.CompareTag("Player2"))
         {
             CauseEffect(rigidbody_PlayerTwo);
+            m_myManager.CollectAt(m_myManagerIndex);
         }
     }
 
--- twoplayergame/Assets/Scripts/PowerUps/PowerUpManager.cs
+++ twoplayergame/Assets/Scripts/PowerUps/PowerUpManager.cs
@@ -4,26 +4,77 @@ using UnityEngine;
 
 public class PowerUpManager : MonoBehaviour {
 
-    public GameObject[] powerUpPrefabList;
-    public const int MAX_POWERUPS = 10;
+    [SerializeField] private PowerUp[] m_powerUpPrefabList;
+    [SerializeField] private int m_maxPowerUps = 100;
+    [SerializeField] private int m_maxFieldPowerUps = 5;
+    [SerializeField] private Vector3[] m_spawnPositions;
+    [SerializeField] private float m_spawnDelay = 1.0f;
 
-    private GameObject[] powerUps = new GameObject[MAX_POWERUPS];
-    private int powerUpTracker = 0;
+    private PowerUp[] m_powerUps;
+    private bool[] m_spawnPositionsOpen;
+    private float m_timer;
+    private int m_powerUpCount = 0;
+    void Awake () {
+        m_powerUps = new PowerUp[m_maxPowerUps];
+        m_spawnPositionsOpen = new bool[m_maxPowerUps];
+        for (int j = 0; j < m_maxPowerUps; ++j) {
+            m_powerUps[j] = Instantiate(GetRandomPrefab(), this.transform).GetComponent<PowerUp>();
+            m_powerUps[j].HookIntoManager(this, "powerup|" + j, j);
+            m_powerUps[j].transform.parent = transform;
+            m_powerUps[j].transform.localPosition = Vector3.zero;
+            m_powerUps[j].gameObject.SetActive(false);
+            m_spawnPositionsOpen[j] = true;
+        }
 
-	// Use this for initialization
-	void Start () {
-		for(int j = 0; j < MAX_POWERUPS;) {
-            powerUps[j] = Instantiate(GetRandomPrefab(), this.transform);
-            powerUps[j].SetActive(false);
+        m_timer = m_spawnDelay;
+	}
+
+    private GameObject GetRandomPrefab()
+    {
+        return m_powerUpPrefabList[Random.Range(0, m_powerUpPrefabList.Length)].gameObject;
+    }
+
+    private Vector3 GetRandomSpawnPos()
+    {
+        return m_spawnPositions[Random.Range(0, m_spawnPositions.Length)];
+    }
+    
+    public void CollectAt(int idx)
+    {
+        --m_powerUpCount;
+        m_powerUps[idx].transform.position = Vector3.zero;
+        m_powerUps[idx].gameObject.SetActive(false);
+        m_spawnPositionsOpen[idx] = true;
+    }
+
+    private void Update () {
+        if (PauseManager.OnlyOne.Paused()) { return; }
+
+        m_timer -= Time.deltaTime;
+
+        if (m_timer <= 0.0f)
+        {
+            m_timer += m_spawnDelay;
+            SpawnPowerUp();
         }
 	}
 
-    GameObject GetRandomPrefab() {
-        return powerUpPrefabList[Random.Range(0, powerUpPrefabList.Length)];
+    private void SpawnPowerUp()
+    {
+        if (m_powerUpCount < m_maxFieldPowerUps)
+        {
+            ++m_powerUpCount;
+
+            int firstSlot = 0;
+            for (int i = 0; i < m_maxPowerUps; ++i)
+            {
+                if (m_spawnPositionsOpen[i]) { firstSlot = i; break; }
+            }
+
+            m_spawnPositionsOpen[firstSlot] = false;
+            PowerUp toAdd = m_powerUps[firstSlot];
+            toAdd.gameObject.SetActive(true);
+            toAdd.gameObject.transform.position = GetRandomSpawnPos();
+        }
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
