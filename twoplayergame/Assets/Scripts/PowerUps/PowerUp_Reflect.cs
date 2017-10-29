ab08e58739e3ea64726ea482267011e65cef2338
Sen <keima.ayasaki@gmail.com>
2017-10-29 12:56:46 -0600

    I made a bumper power up

---
+++ twoplayergame/Assets/Scripts/PowerUps/PowerUp_Reflect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PowerUp_Reflect : PowerUp
+{
+    
+    [SerializeField] [Range(0.0f, 3.0f)] private float scalar = 1.5f;
+
+    public override void CauseEffect(Rigidbody otherRigidBody)
+    {
+        otherRigidBody.velocity = new Vector3(-otherRigidBody.velocity.x * scalar, -otherRigidBody.velocity.y * scalar, -otherRigidBody.velocity.z * scalar);
+        this.gameObject.SetActive(false);
+    }
+}
